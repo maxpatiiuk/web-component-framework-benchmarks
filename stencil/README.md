9cc389ffe47ceff591b71489cc465525f8d5910a
Max Patiiuk <max@patii.uk>
2024-06-04 19:59:56 -0700

    docs(Calcite): create reproduction for missing JSX typing

--- stencil/README.md
+++ stencil/README.md
@@ -1,5 +1,15 @@
 # Stencil
 
+Calcite component types for Stencil JSX do not work by default (not just
+Calcite's fault - seems to apply to all libraries written using Stencil)
+
+Open [./src/components/root/root.tsx](./src/components/root/root.tsx) in your
+IDE to thee example.
+
+I developed a workaround. See
+[src/workaround.ts.disabled](./src/workaround.ts.disabled) (rename that file to
+`workaround.ts` to enable it and see correct type-checking).
+
 ## Installation
 
 1. Clone this repository and run `npm install` in the current directory
--- stencil/src/components/root/root.tsx
+++ stencil/src/components/root/root.tsx
@@ -1,4 +1,13 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, Fragment } from '@stencil/core';
+
+/**
+ * Calcite component types for Stencil JSX do not work by default
+ * (not just Calcite's fault - seems to apply to all libraries written using Stencil)
+ *
+ * I developed a workaround. See "../../workaround.ts.disabled" (rename that
+ * file to "workaround.ts" to enable it and see correct type-checking).
+ *
+ */
 
 @Component({
   tag: 'stencil-root',
@@ -6,6 +15,30 @@ import { Component, h } from '@stencil/core';
 })
 export class StencilRoot {
   render() {
-    return <stencil-counter initialCount={10} />;
+    /**
+     * If you hover over "calcite-button", it is typed as "any".
+     * None of the props are type-checked. You don't get autocomplete
+     * either.
+     */
+    return (
+      <>
+        <calcite-button
+          // If you get a TypeScript error here, that means types are loaded:
+          alignment={1.5}
+          unknownProp={1}
+          title="1"
+        />
+        <calcite-button
+          // If you get a TypeScript error here, that means types are loaded:
+          unknownProp={1}
+          title="1"
+        />
+        1
+        <calcite-does-not-exist />
+        {/** TypeScript errors for native elements are always working: */}
+        <div unknownProp={console.log} />
+        <div title={1} />
+      </>
+    );
   }
 }
